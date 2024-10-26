5158f424c3b3c41146be7ec1deabbf19dea06bb2
MVP Plus <equipe.mvpplus@gmail.com>
2024-10-25 23:27:42 -0300

    test: should mint

--- test/TheSeed.test.ts
+++ test/TheSeed.test.ts
@@ -17,13 +17,37 @@ describe("TheSeed", function () {
     return { seed, owner, otherAccount };
   }
 
-  describe("Deployment", function () {
+  describe("The Seeds Tests", function () {
 
     it("Should be name", async function () {
       const { seed, owner, otherAccount } = await loadFixture(deployFixture);
 
       expect(await (seed as any).name()).to.equal("The Seed");
       
+    });
+
+    it("Should be symbol", async function () {
+      const { seed, owner, otherAccount } = await loadFixture(deployFixture);
+
+      expect(await (seed as any).symbol()).to.equal("SEED");
+      
+    });
+
+    it("Should mint", async function () {
+      const { seed, owner, otherAccount } = await loadFixture(deployFixture);
+
+      await seed.mint();
+
+      const balance = await seed.balanceOf(owner.address);
+      const tokenId = await seed.tokenByIndex(0);
+      const ownerOf = await seed.ownerOf(tokenId);
+      const ownerTokenId = await seed.tokenOfOwnerByIndex(owner.address, tokenId);
+      const totalSupply = await seed.totalSupply();
+
+      expect(balance).to.equal(1);
+      expect(tokenId).to.equal(ownerTokenId);
+      expect(ownerOf).to.equal(owner.address);
+      expect(totalSupply).to.equal(1);
     });
 
   });
