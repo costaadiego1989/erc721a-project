391d26a5e0594bb518278bf38eacce4733d6085d
MVP Plus <equipe.mvpplus@gmail.com>
2024-10-28 21:58:46 -0300

    config deploy file

---
+++ ignition/modules/TheSeed.ts
@@ -0,0 +1,13 @@
+// This setup uses Hardhat Ignition to manage smart contract deployments.
+// Learn more about it at https://hardhat.org/ignition
+
+import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+
+const TheSeed = buildModule("TheSeed", (m) => {
+
+  const theSeed = m.contract("TheSeed" { });
+
+  return { theSeed };
+});
+
+export default TheSeed;
